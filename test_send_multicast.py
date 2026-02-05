54c56c3f764cfced08c2fbdb16d931910f9b84cd
Ludvig Magnusson <ludmagn@student.chalmers.se>
2026-02-05 12:03:00 +0100

    send improvements

--- test_send_multicast.py
+++ test_send_multicast.py
@@ -8,21 +8,23 @@ def get_ipv4_interfaces():
     for iface, addrs in psutil.net_if_addrs().items():
         for addr in addrs:
             if addr.family == AF_INET:
-                if not addr.address.startswith("127."):
-                    ips.append(addr.address)
+                # if not addr.address.startswith("127."):
+                ips.append(addr.address)
     return ips
 
 print(get_ipv4_interfaces())
 
 MCAST_GRP = "239.255.42.99"
-PORT = 9991
-
-s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
-s.setsockopt(IPPROTO_IP, IP_MULTICAST_TTL, 1)
+PORT = 9992
 
 for ip in get_ipv4_interfaces():
     try:
+        s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+        s.setsockopt(IPPROTO_IP, IP_MULTICAST_TTL, 1)
         s.setsockopt(IPPROTO_IP, IP_MULTICAST_IF, inet_aton(ip))
-        s.sendto(b"hello", ("239.255.42.99", 9991))
+        s.sendto(b"hello", (MCAST_GRP, PORT))
+        s.close()
+        
+        print("try send " + ip)
     except:
-        pass
+        print(ip + " failed")
