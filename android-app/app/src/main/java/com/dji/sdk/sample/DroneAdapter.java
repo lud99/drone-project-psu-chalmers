312caa805ca4c1a26fbd4dc2a007d110c48e283e
Isac Lilja <isac.lilja@hotmail.com>
2026-02-11 15:53:03 +0100

    Added DroneAdapter interface

---
+++ android-app/app/src/main/java/com/dji/sdk/sample/DroneAdapter.java
@@ -0,0 +1,53 @@
+import org.json.JSONObject;
+
+
+public interface DroneAdapter {
+
+    public static Telemetry {
+        String droneID;
+        double lat;
+        double long;
+        float alt;
+        int heading;
+        float speed;
+        int batteryPercent;
+    }
+
+    public static RegistrationData {
+        String droneType;
+        String model;
+        String droneID;
+        Capabilities capabilities;
+    }
+
+    public static class Capabilities {
+        public Camera camera = null;
+        public Led led = null;
+        public boolean spotlight = false;
+        public boolean speaker = false;
+        public int maxSpeed = null;
+
+        public static class Camera {
+            public String aspect_ratio = null;
+            public double horizontal_fov = null;
+            public int resolution = null;
+        }
+
+        public static class Led {
+            public String[] colors = {}; // Will likely have to changed
+        }
+    }
+
+    void goTo(double lat, double lon, float alt, Integer heading, String missionID, int taskIndex);
+    void angleCamera(float pitch, float yaw, float transitionTime, String missionID, int taskIndex);
+    void playAudio(String file, float volume, Integer durationSeconds, String missionID, int taskIndex);
+    void goHome(int taskIndex);
+    void led(String color, String pattern, Integer durationSeconds, String missionID, int taskIndex)
+    void spotlight(String pattern, int durationSecond, String missionID, int taskIndex)
+
+    void abortTask(String missionID, int taskIndex);
+    void land(String missionID, int taskIndex);
+
+    JSONObject getTelemetry(); 
+    JSONObject getRegistrationData();
+}
