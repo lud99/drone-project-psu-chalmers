27af9684d5eeefa70a34102fe2af11e69202fe8e
Isac Lilja <isac.lilja@hotmail.com>
2026-02-17 11:44:29 +0100

    Fixed compiling errors in drone adapter

--- android-app/app/src/main/java/com/dji/sdk/sample/DroneAdapter.java
+++ android-app/app/src/main/java/com/dji/sdk/sample/DroneAdapter.java
@@ -1,3 +1,5 @@
+package com.dji.sdk.sample;
+
 /**
      * Contains the structure for the adapter for each drone type.
      * Each drone type should have it own class "<drone_type>Adapter" Eg. DJIAdapter/MavlinkAdapter that follows this structure.
@@ -8,34 +10,35 @@
 
 public interface DroneAdapter {
 
-    public static Telemetry {
+    class Telemetry {
         String droneID;
         double lat;
-        double long;
+        double lon;
         float alt;
         int heading;
         float speed;
         int batteryPercent;
     }
 
-    public static RegistrationData {
+    static class RegistrationData {
         String droneType;
         String model;
         String droneID;
         Capabilities capabilities;
     }
 
-    public static class Capabilities {
+    static class Capabilities {
         public Camera camera = null;
         public Led led = null;
         public boolean spotlight = false;
         public boolean speaker = false;
-        public int maxSpeed = null;
+        public Integer maxSpeed = null;
 
         public static class Camera {
-            public String aspect_ratio = null;
-            public double horizontal_fov = null;
-            public int resolution = null;
+            public Float aspect_ratio = null;
+            public Float horizontal_fov = null;
+            public Integer resolution_height = null;
+            public Integer resolution_width = null;
         }
 
         public static class Led {
@@ -44,15 +47,18 @@ public interface DroneAdapter {
     }
 
     void goTo(double lat, double lon, float alt, Integer heading, String missionID, int taskIndex);
-    void angleCamera(float pitch, float yaw, float transitionTime, String missionID, int taskIndex);
+    void angleCamera(float pitch, float yaw, Float transitionTime, String missionID, int taskIndex);
     void playAudio(String file, float volume, Integer durationSeconds, String missionID, int taskIndex);
-    void goHome(int taskIndex);
-    void led(String color, String pattern, Integer durationSeconds, String missionID, int taskIndex)
-    void spotlight(String pattern, int durationSecond, String missionID, int taskIndex)
+    void goHome(String missionID, int taskIndex);
+    void led(String color, String pattern, Integer durationSeconds, String missionID, int taskIndex);
+    void spotlight(float brightness, Integer durationSeconds, String missionID, int taskIndex);
 
     void abortTask(String missionID, int taskIndex);
     void land(String missionID, int taskIndex);
 
     Telemetry getTelemetry(); 
     RegistrationData getRegistrationData();
+    
+    void pushTaskComplete(String missionID, int taskIndex);
+    void pushTaskFailed(String missionID, int taskIndex);
 }
